e8557f0d31bf142c5469992858aabc3d938c05ff
Vinicius Inocêncio <vicnicius2009@gmail.com>
2022-07-19 12:24:40 -0300

    Feat: adding movies ListView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:the_movie_session/controllers/movie_controller.dart';
+import 'package:the_movie_session/models/movies_models.dart';
+import 'package:the_movie_session/repositories/movies_repository_imp.dart';
+import 'package:the_movie_session/services/dio_service_imp.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final MovieController _controller = MovieController(
+    MoviesRepositoryImp(DioServiceImp()),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: ValueListenableBuilder<Movies?>(
+            valueListenable: _controller.movies,
+            builder: (_, movies, __) {
+              return movies != null
+                  ? ListView.builder(
+                      itemCount: movies.results.length,
+                      itemBuilder: (_, idx) =>
+                          Text(movies.results[idx].title))
+                  : Container(
+                  height: 100,
+                  color: Colors.blue,
+              );
+            })
+    );
   }
 }
